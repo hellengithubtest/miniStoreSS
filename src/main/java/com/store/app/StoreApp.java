4f8bd3f9c947ffbd2ba5f48fe15af278a674f0d9
ElenaPavlova <hell618lena@gmail.com>
2019-08-30 15:26:31 +0500

    jndi

--- src/main/java/com/store/app/StoreApp.java
+++ src/main/java/com/store/app/StoreApp.java
@@ -3,10 +3,11 @@ package com.store.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(exclude = IntegrationAutoConfiguration.class)
 public class StoreApp {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApp.class);
